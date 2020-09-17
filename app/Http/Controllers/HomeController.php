f6dc9f75fd904e25adcef1f7e46560dcba13c8e8
sl <s.leyral@gmail.com>
2020-09-17 22:35:19 +0200

    creation ProductController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
@@ -23,6 +15,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = Product::whereActive(true)->get();
+
+        return view('home', compact('products'));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static whereActive(bool $true)
+ */
 class Product extends Model
 {
     protected $fillable = [
