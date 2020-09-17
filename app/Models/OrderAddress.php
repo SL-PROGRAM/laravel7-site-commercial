ab84f51c8810a23e6704c73ff23c465df8d760e3
sl <s.leyral@gmail.com>
2020-09-17 15:32:15 +0200

    creation model orderAddress

---
+++ app/Models/OrderAddress.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderAddress extends Model
+{
+    protected $fillable = [
+        'name',
+        'firstname',
+        'professionnal',
+        'civility',
+        'company',
+        'address',
+        'addressbis',
+        'bp',
+        'postal',
+        'city',
+        'phone',
+        'country_id',
+        'facturation',
+    ];
+}
